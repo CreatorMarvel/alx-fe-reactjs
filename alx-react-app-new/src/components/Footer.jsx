7feea4db9a2d57934617f8750f8e590a52fd4a12
Lawrence Mile <Laurancemile@gmail.com>
2024-11-06 17:43:27 +0200

    New App Created

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+	return (
+		<footer
+			style={{
+				backgroundColor: "lightblue",
+				padding: "1rem",
+				marginTop: "1rem",
+			}}
+		>
+			<p>2023 City Lovers</p>
+		</footer>
+	);
+}
+
+export default Footer;
